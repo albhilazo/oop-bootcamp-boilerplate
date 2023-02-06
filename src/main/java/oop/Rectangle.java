be237156c02a7b6d45ec6bec8a2da21945e2ddfc
Albert Hilazo <ahilazo@thoughtworks.com>
2023-02-06 11:40:17 +0100

    Rectangle with perimeter and area

---
+++ src/main/java/oop/Rectangle.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Rectangle {
+
+    private final float width;
+    private final float length;
+
+    public Rectangle(float width, float length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public float perimeter() {
+        return 2*(width + length);
+    }
+
+    public float area() {
+        return width*length;
+    }
+}
---
+++ src/test/java/oop/RectangleTest.java
@@ -0,0 +1,30 @@
+package oop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class RectangleTest {
+
+    private Rectangle rectangle;
+
+    @BeforeEach
+    public void setup() {
+        this.rectangle = new Rectangle(10, 20);
+    }
+
+    @Test
+    public void itShouldProvidePerimeter() {
+        float perimeter = rectangle.perimeter();
+        assertEquals(60, perimeter);
+    }
+
+    @Test
+    public void itShouldProvideArea() {
+        float area = rectangle.area();
+        assertEquals(200, area);
+    }
+
+}
