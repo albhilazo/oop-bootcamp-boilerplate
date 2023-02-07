8d77246d649fbe5e0b2376a92b1b1cc57f66da01
anca <anca.munteanurotaru@thoughtworks.com>
2023-02-07 12:03:10 +0100

    Add conversion to yards

--- src/main/java/oop/UnitConverter.java
+++ src/main/java/oop/UnitConverter.java
@@ -1,8 +1,9 @@
 package oop;
 
-public class Distance {
+public class UnitConverter {
 
     private final static double FOOT_TO_METER_CONVERSION = 0.3048;
+    private final static double INCH_TO_YARD_CONVERSION = 0.0278;
 
     public static double toMeters(double feet) {
         return feet * FOOT_TO_METER_CONVERSION;
@@ -12,4 +13,7 @@ public class Distance {
         return meters / FOOT_TO_METER_CONVERSION;
     }
 
+    public static double toYards(double inches) {
+        return inches * INCH_TO_YARD_CONVERSION;
+    }
 }
--- src/test/java/oop/UnitConverterTest.java
+++ src/test/java/oop/UnitConverterTest.java
@@ -4,20 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DistanceTest {
+public class UnitConverterTest {
 
     private final double DELTA_TOLERANCE = 0.01d;
 
     @Test
     public void itShouldConvertFeetToMeters() {
-        double actualMeters = Distance.toMeters(5);
+        double actualMeters = UnitConverter.toMeters(5);
         assertEquals(1.52, actualMeters, DELTA_TOLERANCE);
     }
 
     @Test
     public void itShouldConvertMetersToFeet() {
-        double actualFeet = Distance.toFeet(1);
+        double actualFeet = UnitConverter.toFeet(1);
         assertEquals(3.28, actualFeet, DELTA_TOLERANCE);
     }
 
+    @Test
+    public void itShouldConvertInchesToYards() {
+        double actualYards = UnitConverter.toYards(5);
+        assertEquals(0.1389, actualYards, DELTA_TOLERANCE);
+    }
 }
