d22f876fe5f75d6a7645a75cb9bd08a82c1c8e9e
Evan Rees <evanrt@thoughtworks.com>
2023-02-09 15:20:25 +0100

    add constants

--- src/main/java/oop/greeter/Greeter.java
+++ src/main/java/oop/greeter/Greeter.java
@@ -4,6 +4,9 @@ import java.util.Locale;
 
 public class Greeter {
 
+    private static final String DEFAULT = "Hello";
+    private static final String MORNING = "Good morning";
+
     public String greet(String name) {
         String trimmedName = name.trim();
         String capitalizedName = capitalizeFirstLetter(trimmedName);
